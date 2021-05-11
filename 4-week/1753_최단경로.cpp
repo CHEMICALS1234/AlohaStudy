465076fdb96316197bff8c2ab5dcb0a7c0e93c5c
2021031685 <ysmbest1@hanyang.ac.kr>
2021-05-11 12:01:14 +0900

    Added 4-week

---
+++ 4-week/1753_최단경로.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <queue>
+#include <tuple>
+
+using namespace std;
+
+const int INF = 2'000'000'009;
+const int V_MAX = 1'000'006;
+
+typedef pair<int, int> Edge;
+vector<Edge> edges[V_MAX];
+int dist[V_MAX];
+
+void Dijkstra(int start)
+{
+  fill(dist, dist + V_MAX, INF);
+  priority_queue<Edge> PQ;
+  PQ.push({0, start});
+
+  while (!PQ.empty())
+  {
+    int u, v, d, w;
+    tie(d, u) = PQ.top();
+    PQ.pop();
+
+    if (dist[u] < INF)
+      continue;
+    dist[u] = -d;
+
+    for (Edge e : edges[u])
+    {
+      tie(v, w) = e;
+      if (dist[v] < INF)
+        continue;
+      PQ.push({d - w, v});
+    }
+  }
+}
+
+int V, E, K;
+
+int main()
+{
+  int a, b, w;
+  scanf("%d %d %d", &V, &E, &K);
+  for (int e = 1; e <= E; e++)
+  {
+    scanf("%d %d %d", &a, &b, &w);
+    edges[a].push_back({b, w});
+  }
+
+  Dijkstra(K);
+  for (int v = 1; v <= V; v++)
+  {
+    if (dist[v] == INF)
+      printf("INF\n");
+    else
+      printf("%d\n", dist[v]);
+  }
+  return 0;
+}
