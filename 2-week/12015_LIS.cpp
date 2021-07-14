c52b7bdda724d7cbcab5cb8b99d3c4e0f449eba4
2021031685 <ysmbest1@hanyang.ac.kr>
2021-07-14 21:18:27 +0900

    modified formatting

--- 2-week/12015_LIS.cpp
+++ 2-week/12015_LIS.cpp
@@ -4,8 +4,7 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(0);
   cout.tie(0);
@@ -13,19 +12,14 @@ int main()
   int n;
   cin >> n;
   int v[n];
-  for (int i = 0; i < n; i++)
-  {
+  for (int i = 0; i < n; i++) {
     cin >> v[i];
   }
-  for (int i = 0; i < n; i++)
-  {
+  for (int i = 0; i < n; i++) {
     int idx = (lower_bound(LIS.begin(), LIS.end(), v[i]) - LIS.begin());
-    if (idx == LIS.size())
-    {
+    if (idx == LIS.size()) {
       LIS.push_back(v[i]);
-    }
-    else
-    {
+    } else {
       LIS[idx] = v[i];
     }
   }
