6cf923b6937a7619b734145835ccf00d4717f085
2021031685 <ysmbest1@hanyang.ac.kr>
2021-07-21 19:06:01 +0900

    knapsack

--- 6-week/12865_평범한베낭.cpp
+++ 6-week/12865_평범한베낭.cpp
@@ -2,23 +2,31 @@
 
 using namespace std;
 
-const int N_MAX = 100;
-const int W_MAX = 100'000;
+const int N_MAX = 100 + 10;
+const int W_MAX = 100'000 + 10;
+
+int dp[N_MAX][W_MAX];
 
 int main() {
-  ;
   int N, W;
-  int _w[N_MAX + 1], c[N_MAX];
+  scanf("%d %d", &N, &W);
 
-  int dp[N_MAX + 1][W_MAX + 1];
+  int _w[N_MAX], c[N_MAX];
 
-  for (int k = 0; k < N; k++) {
-    for (int w = 0; w < W; w++) {
+  for (int i = 1; i <= N; i++) {
+    scanf("%d %d", &_w[i], &c[i]);
+  }
+
+  for (int k = 1; k <= N; k++) {
+    for (int w = 0; w <= W; w++) {
       if (w >= _w[k])
-        dp[k + 1][w] = max(dp[k][w], dp[k][w - _w[k]] + c[k]);
+        dp[k][w] = max(dp[k - 1][w], dp[k - 1][w - _w[k]] + c[k]);
       else
-        dp[k + 1][w] = dp[k][w];
+        dp[k][w] = dp[k - 1][w];
     }
   }
+
+  printf("%d\n", dp[N][W]);
+
   return 0;
 }
