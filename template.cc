8926fd29ffe40f166877341f2938de60c0a99fc7
monognuisy <ysmsmart1@gmail.com>
2022-07-01 10:43:28 +0900

    templates

---
+++ template.cc
@@ -0,0 +1,49 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+
+#include <chrono>
+#include <random>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> p32;
+typedef pair<ll, ll> p64;
+typedef pair<double, double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define ln "\n"
+#define endl "\n"
+#define dbg(x) cout << #x << " = " << x << ln
+#define INF 2e18
+#define fast_cin()                  \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);                    \
+  cout.tie(NULL)
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+constexpr double eps = 1e-12;
+
+int dy[] = {0, 1, 0, -1};
+int dx[] = {1, 0, -1, 0};
+
+void solve() {}
+int main() {
+  fast_cin();
+
+  return 0;
+}
